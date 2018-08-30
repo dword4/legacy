8e980baf1cc4cbef752f53013b86f534981cacdb
Drew Hynes <drew.hynes@gmail.com>
2018-08-30 13:58:10 -0400

    added empty files

---
+++ melchior/ingest.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+""" run.py: Runs the various components that comprise the Melchior tool """
+
+__author__ = "Drew Hynes"
+__copyright__ = "Copyright 2018"
+__credits__ = ["Drew Hynes"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Drew Hynes"
+__email__ = "drew.hynes@gmail.com"
+__status__ = "In Development"
+
+"""
+    The idea for now is to have one class per Ingest type, as it expands it may be necessary to move to separate files
+"""
+class IngestFile:
+
+
+
---
+++ melchior/run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+""" run.py: Runs the various components that comprise the Melchior tool """
+
+__author__ = "Drew Hynes"
+__copyright__ = "Copyright 2018"
+__credits__ = ["Drew Hynes"]
+__license__ = "MIT"
+__version__ = "1.0.1"
+__maintainer__ = "Drew Hynes"
+__email__ = "drew.hynes@gmail.com"
+__status__ = "In Development"
